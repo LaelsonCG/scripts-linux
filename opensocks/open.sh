f8d78fde376fd7ec0dff849a88d514d72346d07d
LaelsonCG <laelsoncoutinho@gmail.com>
2021-11-12 20:32:05 -0300

    Update open.sh

--- opensocks/open.sh
+++ opensocks/open.sh
@@ -1,3 +1,3 @@
 cd /etc && rm -r openvpn
 wget https://github.com/LaelsonCG/scripts-linux/raw/main/opensocks/openvpn.zip
-unzip openvpn.zip
+unzip openvpn.zip && rm openvpn.zip && cd /etc/SSHPlus/ &&
