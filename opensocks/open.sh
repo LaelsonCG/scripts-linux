8f31d10987dd58538ab2318f35aa16897d98f83d
LaelsonCG <laelsoncoutinho@gmail.com>
2021-11-12 21:05:54 -0300

    Update open.sh

--- opensocks/open.sh
+++ opensocks/open.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 cd /etc/ && rm -r openvpn &&
 wget https://github.com/LaelsonCG/scripts-linux/raw/main/opensocks/openvpn.zip &&
 unzip openvpn.zip &&
@@ -7,3 +6,11 @@ rm openvpn.zip &&
 cd /etc/SSHPlus/ &&
 rm open.py &&
 wget https://raw.githubusercontent.com/LaelsonCG/scripts-linux/main/opensocks/open.py &&
+chmod 777 open.py
+cd /root && rm SSHPLUS.ovpn &&
+wget https://raw.githubusercontent.com/LaelsonCG/scripts-linux/main/opensocks/SSHPLUS.ovpn &&
+clear && print -ne "Servidor OpenVPN + Socks Instalado com sucesso ✅" && sleep 2 &&
+print ""
+print -ne "Pronto, agora reinicie o servidor" &&
+print -ne "para que as alterações entre em vigor!" &&
+rm open.sh &&
